0dc7ea964cba717d5d98d9715c26fe1f58271a8a
Lucas Rocha <lucasrocha.dv@gmail.com>
2022-03-20 12:50:48 -0300

    Add project files.

---
+++ Entities/JobVacancy.cs
@@ -0,0 +1,12 @@
+﻿namespace DevJobs.API.Entities
+{
+    public class JobVacancy
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Company { get; private set; }
+        public bool IsRemote { get; private set; }
+        public string SalaryRange { get; private set; }
+    }
+}
