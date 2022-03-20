df1cc7e8cddfe37b8b1524411cadd7045f759834
Lucas Rocha <lucasrocha.dv@gmail.com>
2022-03-20 13:08:39 -0300

    Add JobVacanciesController

---
+++ Controllers/JobVacanciesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevJobs.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobVacanciesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put()
+        {
+            return NoContent();
+        }
+    }
+}
